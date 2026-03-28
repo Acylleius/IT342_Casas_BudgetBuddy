63f91a2ffdab9e89c68d841fd4963cc02143c082
LAB13_USER ON WS25 <L13Y17W25@LabHCI.net>
2026-03-28 17:56:33 +0800

    Removed extra folder

--- IT342_Casas_BudgetBuddy-main/mobile/app/src/main/java/com/budgetbuddy/mobile/model/RegisterRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.budgetbuddy.mobile.model
-
-data class RegisterRequest(
-    val firstname: String,
-    val lastname: String,
-    val email: String,
-    val password: String
-)
--- IT342_Casas_BudgetBuddy-main/mobile/app/src/main/java/com/budgetbuddy/mobile/util/SessionManager.kt
+++
@@ -1,4 +0,0 @@
-package com.budgetbuddy.mobile.util
-
-class SessionManager {
-}
